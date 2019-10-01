8e5e13d88d5fde28a35edf23beff26e244df8bb9
steven <stevenjmudie@gmail.com>
2019-10-01 17:40:32 +1000

    updated

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const btnClear = document.getElementById("btnClear");
 function calculate() {
   let length = Array.from(document.querySelectorAll(".cutLength").values()); //gets all length values as nodelist
   let quantity = Array.from(document.querySelectorAll(".quantity").values()); //same with quantities
-
   let inputs = [];
+  let sum = [];
 
   //gets values and pushes into array of objects with properties defining number and quantities
   for (i = 0; i < length.length; i++) {
@@ -19,6 +19,23 @@ function calculate() {
   }
 
   console.log(inputs);
+
+  //adds each cut length input to each other and returns an array of objects
+  for (i = 0; i < inputs.length; i++) {
+    for (p = 0; p < inputs.length; p++) {
+      sum.push({
+        firstNum: inputs[i].length,
+        secondNum: inputs[p].length,
+        total: inputs[i].length + inputs[p].length
+      });
+    }
+  }
+  console.log(sum);
+
+  //uses function to filter out duplicate values
+  let filteredSum = removeDuplicates(sum, "total");
+
+  console.log(filteredSum);
 }
 
 //adds new input box
@@ -49,3 +66,17 @@ btnClear.addEventListener("click", () => {
     output.textContent = "";
   }
 });
+
+function removeDuplicates(originalArray, prop) {
+  var newArray = [];
+  var lookupObject = {};
+
+  for (var i in originalArray) {
+    lookupObject[originalArray[i][prop]] = originalArray[i];
+  }
+
+  for (i in lookupObject) {
+    newArray.push(lookupObject[i]);
+  }
+  return newArray;
+}
