18836d3813e52215587ff3fe8aa70ad3d67a144e
steven <stevenjmudie@gmail.com>
2019-10-30 21:59:32 +1100

    still issues with duplicate values after array has been filtered if the quantity is 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let materialLength = document.querySelector("#materialLength");
+let materialLength = document.getElementById("materialLength");
 const btnCalculate = document.getElementById("btnCalculate");
 const btnAdd = document.getElementById("btnAdd");
 const output = document.getElementById("output");
@@ -11,7 +11,7 @@ function calculate() {
   let sum = [];
 
   //gets values and pushes into array of objects with properties defining number and quantities
-  for (i = 0; i < length.length; i++) {
+  for (let i = 0; i < length.length; i++) {
     inputs.push({
       length: parseInt(length[i].value),
       quantity: parseInt(quantity[i].value)
@@ -21,11 +21,13 @@ function calculate() {
   console.log(inputs);
 
   //adds each cut length input to each other and returns an array of objects
-  for (i = 0; i < inputs.length; i++) {
-    for (p = 0; p < inputs.length; p++) {
+  for (let i = 0; i < inputs.length; i++) {
+    for (let p = 0; p < inputs.length; p++) {
       sum.push({
         firstNum: inputs[i].length,
+        firstQuantity: inputs[i].quantity,
         secondNum: inputs[p].length,
+        secondQuantity: inputs[p].quantity,
         total: inputs[i].length + inputs[p].length
       });
     }
@@ -33,7 +35,13 @@ function calculate() {
   console.log(sum);
 
   //uses function to filter out duplicate values
-  let filteredSum = removeDuplicates(sum, "total");
+  let removeDuplicateSum = removeDuplicates(sum, "total");
+
+  console.log(removeDuplicateSum);
+
+  let filteredSum = removeDuplicateSum.filter(index => {
+    return index.total <= materialLength.value;
+  });
 
   console.log(filteredSum);
 }
